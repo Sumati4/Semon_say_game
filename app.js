ebb682904574ab3fba15f9e9accf6f1222e8754f
Sumati4 <160269212+Sumati4@users.noreply.github.com>
2024-04-08 00:09:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ function btnPress(){
 let allBtns=document.querySelectorAll(".btn");
 for(btn of allBtns){
     btn.addEventListener("click",btnPress);
-    btn.addEventListener("touchstart", btnPress);
+    
 }
 
 
